3cd5d362a321f853096221d679f51f12ef98a345
Pang <102480141@qq.com>
2024-10-23 22:51:50 +0800

    fix timezone bug

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import schedule
 import time
 import datetime
 import logging
+import pytz  # 导入pytz库
 
 
-# snowball1688@126.com   Aa2120212.
+# 用于发送邮件的函数
 def send_email(subject, body, to_email, attachment_path):
-    # 邮件发送者的邮箱和密码
     from_email = "snowball1688@126.com"  # 发送者邮箱
     password = "NKSLFbA3eghYqHUC"  # 发送者邮箱密码
 
@@ -40,7 +40,7 @@ def send_email(subject, body, to_email, attachment_path):
         return
     try:
         # 连接到SMTP服务器并发送邮件
-        server = smtplib.SMTP("smtp.126.com")  # Gmail SMTP服务器
+        server = smtplib.SMTP("smtp.126.com")  # 126邮件的SMTP服务器
         server.starttls()  # 启用TLS
         server.login(from_email, password)  # 登录
         server.sendmail(from_email, to_email, msg.as_string())  # 发送邮件
@@ -48,54 +48,49 @@ def send_email(subject, body, to_email, attachment_path):
     except Exception as e:
         logging.info(f"发送邮件时出现错误: {e}")
     finally:
-        if server:  # 检查server是否已成功创建
+        if 'server' in locals():  # 检查server是否已成功创建
             server.quit()  # 关闭服务器连接
 
 
+# 获取数据并发送邮件的函数
 def getWenCaiAndSendEmail():
     res = pywencai.get(
-        # query="人均持股市值>50万元；股东人数降序排序；近10日区间换手率<15%；流通市值大于100亿；股价在10日均线上方；持股人数小于15万；总市值大于200亿；股价小于100元"
         query="人均持股市值>50万元；股东人数降序排序；近10日区间换手率<15%；流通市值大于100亿；股价在10日均线上方；持股人数小于15万；股价创近一年新高；总市值大于200亿；股价小于100元"
-        # query="人均持股市值>50万元；股东人数降序排序；近10日区间换手率<15%；流通市值大于1000亿；股价在10日均线上方；持股人数小于15万；股价创近一年新高；总市值大于200亿；股价小于100元"
     )
-    current_datetime = datetime.datetime.now()
-    now = current_datetime.strftime("%Y-%m-%d")
-    # 格式化文件名（例如：2024-10-20_15-30-45.txt）
+
+    # 获取当前时间，并生成文件名
+    current_datetime = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
     filename = current_datetime.strftime("%Y-%m-%d_%H-%M-%S") + ".xlsx"
-    if res is None:
-        logging.info(f"no any data{now}")
+    
+    if res is None or len(res) == 0:
+        logging.info(f"没有任何数据 {current_datetime.strftime('%Y-%m-%d')}")
         return
     else:
         res.to_excel(filename)
-        msg = ""
-        for r in res["股票简称"]:
-            msg = msg + r + ","
-        # msg = str(res["股票简称"])
-
-        # 示例日志输出
+        msg = ", ".join(res["股票简称"])  # 将股票简称连接成字符串
         logging.info(f"发送邮件：{msg}")
-        send_email("神股来了", msg, "panghui23@qq.com", "./" + filename)
+        send_email("神股来了", msg, "panghui23@qq.com", f"./{filename}")
 
 
 # 配置日志格式和级别
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-# 示例日志输出
 logging.info("程序启动")
 
-# getWenCaiAndSendEmail()
+
+# 安排每周一到周五的晚上 6 点 18 分执行 getWenCaiAndSendEmail 函数
+def schedule_job():
+    local_tz = pytz.timezone("Asia/Shanghai")
+    now = datetime.datetime.now(local_tz)
+    # logging.info(f"当前时间: {now.strftime('%Y-%m-%d %H:%M')}")  # 输出当前时间
+    
+    # 检查是否是周一到周五的18:18
+    if now.weekday() <= 5 and now.hour == 18 and now.minute == 18:
+        getWenCaiAndSendEmail()
 
 
-# 安排每周一到周五的晚上 6 点 18 分执行 log_message 函数
-schedule.every().monday.at("18:18").do(getWenCaiAndSendEmail)
-schedule.every().tuesday.at("18:18").do(getWenCaiAndSendEmail)
-schedule.every().wednesday.at("18:18").do(getWenCaiAndSendEmail)
-schedule.every().thursday.at("18:18").do(getWenCaiAndSendEmail)
-schedule.every().friday.at("18:18").do(getWenCaiAndSendEmail)
-
+# 每分钟检查一次
 while True:
-    # 示例日志输出
-
-    schedule.run_pending()
-    time.sleep(1)
+    schedule_job()
+    time.sleep(31)  # 每60秒检查一次
