e76d12c6ec0c2841049554a1e1b0690abf687bbc
Pang <102480141@qq.com>
2024-11-09 10:55:16 +0800

    update

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import time
 import datetime
 import logging
 import pytz  # 导入pytz库
+import os
+import pandas as pd
 
 
 # 用于发送邮件的函数
@@ -58,16 +60,24 @@ def getWenCaiAndSendEmail():
         query="人均持股市值>50万元；股东人数降序排序；近10日区间换手率<15%；流通市值大于100亿；股价在10日均线上方；持股人数小于15万；股价创近一年新高；总市值大于200亿；股价小于100元"
     )
 
+    # 检查并创建 'data' 文件夹（如果不存在的话）
+    if not os.path.exists("data"):
+        os.makedirs("data")
     # 获取当前时间，并生成文件名
     current_datetime = datetime.datetime.now(pytz.timezone("Asia/Shanghai"))
-    filename = current_datetime.strftime("%Y-%m-%d_%H-%M-%S") + ".xlsx"
-
+    filename = "./data/" + current_datetime.strftime("%Y-%m-%d_%H-%M-%S") + ".xlsx"
+    countMap = getCount()
     if res is None or len(res) == 0:
         logging.info(f"没有任何数据 {current_datetime.strftime('%Y-%m-%d')}")
         return
     else:
         res.to_excel(filename)
-        msg = ", ".join(res["股票简称"])  # 将股票简称连接成字符串
+        msg = ""
+        for name in res["股票简称"]:
+            print(name + str(countMap.get(name, 0)))
+            msg += name + str(countMap.get(name, 0)) + ","
+
+        # 将股票简称连接成字符串
         logging.info(f"发送邮件：{msg}")
         send_email("神股来了", msg, "panghui23@qq.com", f"./{filename}")
 
@@ -90,18 +100,39 @@ def schedule_job():
     getWenCaiAndSendEmail()
 
 
+def getCount():
+    folder_path = os.path.join(os.getcwd(), "data")  # 这里替换为你的文件夹路径
+    # print(folder_path)
+    # 获取所有 CSV 文件的路径
+    csv_files = [f for f in os.listdir(folder_path) if f.endswith(".xlsx")]
+    # print(csv_files)
+    # 读取所有 CSV 文件并将它们存入一个列表中
+    dataframes = []
+    for file in csv_files:
+        file_path = os.path.join(folder_path, file)
+        df = pd.read_excel(file_path)
+        dataframes.append(df)
+
+    # 如果需要，你可以将所有 DataFrame 合并为一个 DataFrame
+    combined_df = pd.concat(dataframes, ignore_index=True)
+
+    print(combined_df["股票简称"].value_counts())
+    return combined_df["股票简称"].value_counts()
+
+
 # 定义定时任务的时间
-schedule.every().monday.at("18:18").do(schedule_job)
-schedule.every().tuesday.at("18:18").do(schedule_job)
-schedule.every().wednesday.at("18:18").do(schedule_job)
-schedule.every().thursday.at("18:18").do(schedule_job)
-schedule.every().friday.at("18:18").do(schedule_job)
+schedule.every().monday.at("15:18").do(schedule_job)
+schedule.every().tuesday.at("15:18").do(schedule_job)
+schedule.every().wednesday.at("15:18").do(schedule_job)
+schedule.every().thursday.at("15:18").do(schedule_job)
+schedule.every().friday.at("15:18").do(schedule_job)
 # schedule.every().saturday.at("17:21").do(schedule_job)
 
 # 设置每分钟检查一次
 # schedule.every().minute.do(schedule_job)
 # 每分钟检查一次
 # 保持运行状态
+# getWenCaiAndSendEmail()
 if __name__ == "__main__":
     while True:
         schedule.run_pending()
